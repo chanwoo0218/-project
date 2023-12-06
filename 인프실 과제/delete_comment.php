7d4bd2abb13e22531541ac9dfbd9e5e5f74151aa
6thraindrop <152474950+6thraindrop@users.noreply.github.com>
2023-12-06 17:13:55 +0900

    Update delete_comment.php

--- 인프실 과제/delete_comment.php
+++ 인프실 과제/delete_comment.php
@@ -1,19 +1,22 @@
 <?php
-	include("./config.php");
-	session_start();
-	$writer=$_SESSION['nickname'];
+include("./config.php");
+session_start();
+$writer = $_SESSION['nickname'];
 
-	$num=$_GET['num'];
-	$page=$_GET['page'];
-	$reply_num=$_GET['reply_num'];
+$num = $_GET['num'];
+$page = $_GET['page'];
+$reply_num = $_GET['reply_num'];
 
-	$query="DELETE from reply WHERE UID='$reply_num'";
+// 파라미터화된 SQL 쿼리 사용
+$query = "DELETE FROM reply WHERE UID = ?";
+$stmt = mysqli_prepare($con, $query);
+mysqli_stmt_bind_param($stmt, 'i', $reply_num);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_close($stmt);
 
-	mysqli_query($con,$query);
-	mysqli_close($con);
-	echo("<script> 
-		location.href='read.php?page=$page&uid=$num'
-		</script>");
-	
-	
+mysqli_close($con);
+
+echo("<script> 
+    location.href='read.php?page=$page&uid=$num'
+    </script>");
 ?>
